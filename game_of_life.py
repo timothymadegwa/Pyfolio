2c5003a40321fea98a76a6372558d4f9392b450f
timothymadegwa <timothymadegwa@gmail.com>
2019-07-18 23:55:07 +0300

    adding a doc string to the create_game method

--- game_of_life.py
+++ game_of_life.py
@@ -5,6 +5,10 @@ class GameOfLife:
         self.game=[[None for _ in range(self.col)] for _ in range(self.row)]
 
     def create_game(self, one, two, three):
+        """
+        This function creates the array containing the living cells by parsing in 3 tuples for
+        the positions of the living cells
+        """
         self.x1, self.x2=one
         self.y1, self.y2=two
         self.z1, self.z2=three
